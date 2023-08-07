599b9ee01f5fbff711ce96eb68af714171c7163c
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2023-08-06 20:08:20 -0500

    listo para hacer mejoras

--- main.js
+++ main.js
@@ -1,49 +1,71 @@
 import { getEnfermedades } from "./firebase.js";
 
 const contenedorPadre = document.querySelector('.contenedor-Padre')
+
 async function  RenderTarjetas(){
     // esta funcion le asigna la estructura html a cada elemento traido desde firebase
     const queryEnfermedad = await getEnfermedades()
       const arr = []
       queryEnfermedad.forEach((Enfermedad) =>{ 
+
+        //creando los elementos html necesarios
         const enfermedad = Enfermedad.data()
         const div = document.createElement('div')
         const divPhoto = document.createElement('div')
         const divSintomas = document.createElement('div')
         const divDefinicion = document.createElement('div')
         const divbutton = document.createElement('div')
+        const contenedorPhoto = document.createElement('div')
         const h2 =document.createElement('h2')
+        const h2stms =document.createElement('h2')
         const img = document.createElement('img')
         const p = document.createElement('p')
-        const a = document.createElement('button')
+        const btn = document.createElement('button')
+        const btnclose = document.createElement('button')
+        const volverDeinicion = document.createElement('a')
+        const i = document.createElement('i')
 
-        
+        //asignado las clases correspondientes
+        i.setAttribute('class', 'fa-solid fa-xmark fa-2xl')
+        contenedorPhoto.setAttribute('class', '.contenedorPhoto')
         divbutton.setAttribute('class', 'invisible')
         divSintomas.setAttribute('class', 'invisible')
         div.setAttribute('class', 'contenedor-Tarjeta')
         divPhoto.setAttribute('class', 'photo-Enfermedad')
         divDefinicion.setAttribute('class', 'invisible')
-        a.setAttribute('class', 'button')
+        btn.setAttribute('class', 'button')
+        btnclose.setAttribute('class', 'btnClose')
         img.setAttribute('src', enfermedad.img)
         const pText = document.createTextNode(enfermedad.definicion)
         const h2Text =document.createTextNode(enfermedad.nombre)
         const p2Text = document.createTextNode(enfermedad.sintomas)
-        const aText = document.createTextNode('síntomas o consideraciones')
+        const btnText = document.createTextNode('síntomas o consideraciones')
 
-        a.appendChild(aText)
+        h2stms.innerText = 'síntomas o consideraciones'
+        volverDeinicion.innerText = 'Volver a definicion'
+
+
+        btn.appendChild(btnText)
+        btnclose.appendChild(i)
         p.appendChild(pText)
         h2.appendChild(h2Text)
         divPhoto.appendChild(img)
         divPhoto.appendChild(h2)
+        divPhoto.appendChild(btnclose)
+        divSintomas.appendChild(h2stms)
         divSintomas.appendChild(p2Text)
+        divSintomas.appendChild(volverDeinicion)
         divDefinicion.appendChild(p)
-        divbutton.appendChild(a)
+        divbutton.appendChild(btn)
         div.appendChild(divPhoto)
         div.appendChild(divDefinicion)
         div.appendChild(divbutton)
         div.appendChild(divSintomas)
 
-        a.onclick = () => mostrar(divSintomas)
+
+        volverDeinicion.onclick = () => backDefinicion(divSintomas)
+        btnclose.onclick =() => backInicio()
+        btn.onclick = () => mostrar(divSintomas)
         div.onclick = () => expandir(div)
         arr.push(div)
       })
@@ -52,20 +74,51 @@ async function  RenderTarjetas(){
 
   function expandir(contenedor){
     //limpiar
-
+    // recuerda cambiar por selectores en todas las funciones
     contenedorPadre.innerHTML =''
+    const btnclose = document.querySelector('.btnClose')
+
     contenedorPadre.setAttribute('class','contenedor-Tarjeta-Expandir')
     contenedor.setAttribute('class','tarjeta-expandir')
     contenedor.children[0].setAttribute('class', 'contedor-img-expandir')
     contenedor.children[1].setAttribute('class','definicion-expandir')
     contenedor.children[2].setAttribute('class', 'contenedor-btn')
+    
     contenedorPadre.append(contenedor)
   }
+
+
+
   let flag = false
 
   function mostrar(contenedor){
+    const divDefinicion = document.querySelector('.definicion-expandir')
+    const btn = document.querySelector('.button')
+    divDefinicion.style.display = 'none';
     flag = !flag
-    flag ? contenedor.style.display = 'block' : contenedor.style.display = 'none';
+    if(flag){
+      contenedor.style.display = 'block'
+      btn.style.display = 'none'
+    }else{
+      contenedor.style.display = 'none'
+    }
   }
+
+  function backDefinicion(contenedor){
+    const def = document.querySelector('.definicion-expandir')
+    const btn = document.querySelector('.button')
+    
+    contenedor.style.display = 'none'
+    def.style.display = 'block'
+    btn.style.display = 'block'
+    flag =!flag
+  }
+
+  function backInicio() {
+    // limpiar
+    contenedorPadre.innerHTML = ''
+    RenderTarjetas()
+  }
+
 
   RenderTarjetas()

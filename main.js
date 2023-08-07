aa6fae23c4452b9eccf7bfd765d2a21f873cba2c
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2023-08-07 01:22:12 -0500

    mejoras aun no terminadas

--- main.js
+++ main.js
@@ -2,89 +2,132 @@ import { getEnfermedades } from "./firebase.js";
 
 const contenedorPadre = document.querySelector('.contenedor-Padre')
 
+
 async function  RenderTarjetas(){
+  contenedorPadre.innerHTML = ''
     // esta funcion le asigna la estructura html a cada elemento traido desde firebase
     const queryEnfermedad = await getEnfermedades()
       const arr = []
       queryEnfermedad.forEach((Enfermedad) =>{ 
-
-        //creando los elementos html necesarios
         const enfermedad = Enfermedad.data()
+        // //creando los elementos html necesarios
+        // const enfermedad = Enfermedad.data()
+        // const div = document.createElement('div')
+        // const divPhoto = document.createElement('div')
+        // const divSintomas = document.createElement('div')
+        // const divDefinicion = document.createElement('div')
+        // const divbutton = document.createElement('div')
+        // const contenedorPhoto = document.createElement('div')
+        // const h2 =document.createElement('h2')
+        // const h2stms =document.createElement('h2')
+        // const img = document.createElement('img')
+        // const p = document.createElement('p')
+        // const btn = document.createElement('button')
+        // const btnclose = document.createElement('button')
+        // const volverDeinicion = document.createElement('a')
+        // const i = document.createElement('i')
+
+        // //asignado las clases correspondientes
+        // i.setAttribute('class', 'fa-solid fa-xmark fa-2xl')
+        // contenedorPhoto.setAttribute('class', '.contenedorPhoto')
+        // divbutton.setAttribute('class', 'invisible')
+        // divSintomas.setAttribute('class', 'invisible')
+        // div.setAttribute('class', 'contenedor-Tarjeta')
+        // divPhoto.setAttribute('class', 'photo-Enfermedad')
+        // divDefinicion.setAttribute('class', 'invisible')
+        // btn.setAttribute('class', 'button')
+        // btnclose.setAttribute('class', 'btnClose')
+        // img.setAttribute('src', enfermedad.img)
+        // const pText = document.createTextNode(enfermedad.definicion)
+        // const h2Text =document.createTextNode(enfermedad.nombre)
+        // const p2Text = document.createTextNode(enfermedad.sintomas)
+        // const btnText = document.createTextNode('síntomas o consideraciones')
+
+        // h2stms.innerText = 'síntomas o consideraciones'
+        // volverDeinicion.innerText = 'Volver a definicion'
+
+
+        // btn.appendChild(btnText)
+        // btnclose.appendChild(i)
+        // p.appendChild(pText)
+        // h2.appendChild(h2Text)
+        // divPhoto.appendChild(btnclose)
+        // divPhoto.appendChild(img)
+        // divPhoto.appendChild(h2)
+        // divSintomas.appendChild(h2stms)
+        // divSintomas.appendChild(p2Text)
+        // divSintomas.appendChild(volverDeinicion)
+        // divDefinicion.appendChild(p)
+        // divbutton.appendChild(btn)
+        // div.appendChild(divPhoto)
+        // div.appendChild(divDefinicion)
+        // div.appendChild(divbutton)
+        // div.appendChild(divSintomas)
+
+        
+        // volverDeinicion.onclick = () => backDefinicion(divSintomas)
+        // btnclose.onclick =() => backInicio()
+        // btn.onclick = () => mostrar(divSintomas)
+        // div.onclick = () => expandir(div)
+        // arr.push(div)
+
+        //contenedores
         const div = document.createElement('div')
         const divPhoto = document.createElement('div')
-        const divSintomas = document.createElement('div')
-        const divDefinicion = document.createElement('div')
-        const divbutton = document.createElement('div')
-        const contenedorPhoto = document.createElement('div')
-        const h2 =document.createElement('h2')
-        const h2stms =document.createElement('h2')
         const img = document.createElement('img')
-        const p = document.createElement('p')
-        const btn = document.createElement('button')
-        const btnclose = document.createElement('button')
-        const volverDeinicion = document.createElement('a')
-        const i = document.createElement('i')
+        const h2nombre =document.createElement('h2')
 
-        //asignado las clases correspondientes
-        i.setAttribute('class', 'fa-solid fa-xmark fa-2xl')
-        contenedorPhoto.setAttribute('class', '.contenedorPhoto')
-        divbutton.setAttribute('class', 'invisible')
-        divSintomas.setAttribute('class', 'invisible')
-        div.setAttribute('class', 'contenedor-Tarjeta')
-        divPhoto.setAttribute('class', 'photo-Enfermedad')
-        divDefinicion.setAttribute('class', 'invisible')
-        btn.setAttribute('class', 'button')
-        btnclose.setAttribute('class', 'btnClose')
+        //asingacion de clases
+        div.setAttribute('class','contenedorTarjetaMinimal')
+        divPhoto.setAttribute('class','photoEnfermedadMinimal')
+
+        //asignado informacion correspondientes de firebase
         img.setAttribute('src', enfermedad.img)
-        const pText = document.createTextNode(enfermedad.definicion)
-        const h2Text =document.createTextNode(enfermedad.nombre)
-        const p2Text = document.createTextNode(enfermedad.sintomas)
-        const btnText = document.createTextNode('síntomas o consideraciones')
+        const nombreText = document.createTextNode(enfermedad.nombre)
 
-        h2stms.innerText = 'síntomas o consideraciones'
-        volverDeinicion.innerText = 'Volver a definicion'
-
-
-        btn.appendChild(btnText)
-        btnclose.appendChild(i)
-        p.appendChild(pText)
-        h2.appendChild(h2Text)
         divPhoto.appendChild(img)
-        divPhoto.appendChild(h2)
-        divPhoto.appendChild(btnclose)
-        divSintomas.appendChild(h2stms)
-        divSintomas.appendChild(p2Text)
-        divSintomas.appendChild(volverDeinicion)
-        divDefinicion.appendChild(p)
-        divbutton.appendChild(btn)
+        h2nombre.appendChild(nombreText)
+        divPhoto.appendChild(h2nombre)
         div.appendChild(divPhoto)
-        div.appendChild(divDefinicion)
-        div.appendChild(divbutton)
-        div.appendChild(divSintomas)
-
-
-        volverDeinicion.onclick = () => backDefinicion(divSintomas)
-        btnclose.onclick =() => backInicio()
-        btn.onclick = () => mostrar(divSintomas)
-        div.onclick = () => expandir(div)
+        div.onclick = () =>  expandir(enfermedad)
         arr.push(div)
       })
       contenedorPadre.append(...arr)
   }
 
-  function expandir(contenedor){
+  function expandir(enfermedad){
     //limpiar
-    // recuerda cambiar por selectores en todas las funciones
     contenedorPadre.innerHTML =''
-    const btnclose = document.querySelector('.btnClose')
 
-    contenedorPadre.setAttribute('class','contenedor-Tarjeta-Expandir')
-    contenedor.setAttribute('class','tarjeta-expandir')
-    contenedor.children[0].setAttribute('class', 'contedor-img-expandir')
-    contenedor.children[1].setAttribute('class','definicion-expandir')
-    contenedor.children[2].setAttribute('class', 'contenedor-btn')
+    //elementos del detalle  de la tarjeta
+    const contenedorDetalle = document.createElement('div')
+    const imagenDetalle = document.createElement('div')
+    const imgDetalle = document.createElement('img')
+    const h2Detalle = document.createElement('h2')
+    const definicionDetalle = document.createElement('div')
+
+    //asignacion de clases
+    contenedorPadre.setAttribute('class', 'contenedorTarjetaDetalle')
+    contenedorDetalle.setAttribute('class' , 'tarjetaDetalle')
+    imagenDetalle.setAttribute('class', 'imagenDetalle')
+
+    //informacion de firebase
+    imgDetalle.setAttribute('src', enfermedad.img)
+    h2Detalle.innerText = enfermedad.nombre
+
+    imagenDetalle.appendChild(imgDetalle)
+    imagenDetalle.appendChild(h2Detalle)
+    contenedorDetalle.appendChild(imagenDetalle)
+    contenedorPadre.appendChild(contenedorDetalle)
+    console.log('hola')
+  }
+
+  function backInicio() {
+    // limpiar
+      contenedorPadre.innerHTML = ''
+      contenedorPadre.setAttribute('class','contenedor-Padre')
+      RenderTarjetas()
     
-    contenedorPadre.append(contenedor)
   }
 
 
@@ -114,11 +157,7 @@ async function  RenderTarjetas(){
     flag =!flag
   }
 
-  function backInicio() {
-    // limpiar
-    contenedorPadre.innerHTML = ''
-    RenderTarjetas()
-  }
+
 
 
   RenderTarjetas()
